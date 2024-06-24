c97912103cdff04985aa3405ce8d91b9659bca69
brunomanguinho <brunomanguinho@gmail.com>
2024-06-24 08:16:31 -0300

    social media

--- app/ui/navlink.tsx
+++ app/ui/navlink.tsx
@@ -9,16 +9,17 @@ export default function NavLink(
     })
 {
     return (
-        
         <div className="col-3 col-md-12 border link-container rounded">
             <Link 
                 key={value}
                 href={href}
                 className="menu-link" 
             >      
-                {value}
+                <div className="w-100 h-100 content-center">
+                    {value}
+                </div>
+                
             </Link>
-        </div>
-
+        </div>    
     )
 }
--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 import Image from "next/image";
 import NavLink from "./navlink";
+import Link from "next/link";
 
 export default function SideNav(){
     
@@ -34,6 +35,32 @@ export default function SideNav(){
                                 value={link.value}
                             />);
                         })}
+                    </div>
+                    <div className="row">
+                        <div className="col-6 text-center mt-3 ps-5">
+                            <Link
+                                href='https://www.linkedin.com/in/emicles-manguinho-filho-56991734/'
+                            >
+                                <Image 
+                                    src="/imgs/linkedin.png"
+                                    alt="linkedin"
+                                    width={48}
+                                    height={48}
+                                />                               
+                            </Link>
+                        </div>
+                        <div className="col-6 text-center mt-3 pe-5">
+                            <Link
+                                href='mailto:emiclesmang@hotmail.com'
+                            >
+                                <Image 
+                                    src="/imgs/mail.png"
+                                    alt="mail"
+                                    width={48}
+                                    height={48}
+                                />                               
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
